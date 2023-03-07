f5dd665db5dd0112bd11f4653c86ee24b55a57e2
Mike <mike.projektowanie@gmail.com>
2023-03-07 16:10:19 +0100

    function addTask now changes on db.json file

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -14,6 +14,45 @@ export function Tasks() {
     }, [])
 
 
+
+// DODAWANIE ZADANIA:
+
+    // funkcja będzie wykonywać się asynchronicznie
+    const addTask = async (title: string) => {
+
+        // określamy nowe zadanie:
+        const newTask: Task = {
+            id: tasks.length + 1,
+            title,
+            completed: false,
+        };
+
+        // określamy odpowiedź z serwera,
+        // który oczekuje na pojawienie się zadania za pomocą POST
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newTask),
+        });
+
+
+        // sprawdzamy czy odpowiedź jest prawidłowa i tworzymy zmienną data.
+        // Następnie za pomocą useState tworzymy nowe zadanie
+        // i podmieniamy odpowiedź serwera, która wykonała się wyżej.
+        if (response.ok) {
+            const data = await response.json();
+            setTasks([...tasks, data]);
+        }
+    }
+
+// EDTYTOWANIE ZADANIA:
+
+
+
+
+// Zwrotka użytkownika:
     return (
         <div>
             <h2>To-Do List</h2>
@@ -25,6 +64,25 @@ export function Tasks() {
 
                 })}
             </ul>
+
+            <form action="" onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+                e.preventDefault();
+                const titleInput = e.currentTarget.elements.namedItem("title") as HTMLInputElement;
+                
+
+                const title = titleInput.value.trim();
+                console.log("dodaję do listy: " + title)
+                
+                if (title) {
+                    addTask(title);
+                    titleInput.value = "";
+                }
+
+
+            }}>
+                <input type="text" name="title" />
+                <button type="submit">Dodaj</button>
+            </form>
 
         </div>
     )
