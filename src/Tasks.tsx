3450b1ee8575a300994e07c52cc740ab7f750248
Mike <mike.projektowanie@gmail.com>
2023-03-07 15:33:14 +0100

    preparing to add addTask function

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -2,47 +2,31 @@ import { useEffect, useState } from "react"
 import { Task } from "./Task"
 
 
-const API_URL = "https://localhost:3001/tasks"
+const API_URL = "http://localhost:3001/tasks"
 
-export const Tasks = () => {
+export function Tasks() {
     const [tasks, setTasks] = useState<Task[]>([])
 
     useEffect(() => {
         fetch(API_URL)
             .then((res) => res.json())
             .then((data) => setTasks(data))
-    }, []);
-
-    const addTask = async (title: string) => {
-        const newTask: Task = {
-            id: tasks.length + 1,
-            title,
-            completed: false,
-        };
-
-        const res = await fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(newTask),
-        });
-
-        if (res.ok) {
-            const data = await res.json();
-            setTasks([...tasks, data]);
-        }
-    };
+    }, [])
 
 
+    return (
+        <div>
+            <h2>To-Do List</h2>
+            <ul>
+                {tasks.map((task) => {
+                    return (
+                        <li key={task.id}>{task.title}</li>
+                    )
 
-  return (
-    <div>
-        <h2>To-Do List</h2>
-        <ul>
-            
-        </ul>
-    </div>
-  )
+                })}
+            </ul>
+
+        </div>
+    )
 }
 
