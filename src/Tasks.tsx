d3b587ebc47f3318c2c6b412ad66661d11cd192e
Mike <mike.projektowanie@gmail.com>
2023-03-08 09:46:06 +0100

    Function added: editTask, deleteTask.

--- src/Tasks.tsx
+++ src/Tasks.tsx
@@ -4,7 +4,7 @@ import { Task } from "./Task"
 
 const API_URL = "http://localhost:3001/tasks"
 
-export function Tasks() {
+export function Tasks(): JSX.Element {
     const [tasks, setTasks] = useState<Task[]>([])
 
     useEffect(() => {
@@ -15,40 +15,60 @@ export function Tasks() {
 
 
 
-// DODAWANIE ZADANIA:
+// 1. DODAWANIE ZADANIA:
 
     // funkcja będzie wykonywać się asynchronicznie
-    const addTask = async (title: string) => {
+    async function addTask({ title }: { title: string }): Promise<void> {
 
-        // określamy nowe zadanie:
         const newTask: Task = {
             id: tasks.length + 1,
             title,
             completed: false,
-        };
+        }
 
-        // określamy odpowiedź z serwera,
-        // który oczekuje na pojawienie się zadania za pomocą POST
         const response = await fetch(API_URL, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify(newTask),
-        });
-
+        })
 
         // sprawdzamy czy odpowiedź jest prawidłowa i tworzymy zmienną data.
         // Następnie za pomocą useState tworzymy nowe zadanie
         // i podmieniamy odpowiedź serwera, która wykonała się wyżej.
         if (response.ok) {
-            const data = await response.json();
-            setTasks([...tasks, data]);
+            const data = await response.json()
+            setTasks([...tasks, data])
         }
     }
 
-// EDTYTOWANIE ZADANIA:
+// 2. EDTYTOWANIE ZADANIA:
+    async function editTask({ updatedTask }: { updatedTask: Task }): Promise<void> {
+        const response = await fetch(`${API_URL}/${updatedTask.id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedTask),
+        })
 
+        if (response.ok) {
+            const data = await response.json()
+            setTasks(tasks.map((task) => (task.id === data.id ? data : task)))
+        }
+    }
+
+// 3. USUWANIE ZADANIA:
+    async function deleteTask({ taskId }: { taskId: number }): Promise<void> {
+        const response = await fetch(`${API_URL}/${taskId}`, {
+            method: "DELETE",
+        })
+
+        if (response.ok) {
+            setTasks(tasks.filter((task) => (task.id !== taskId)))
+        }
+    }
 
 
 
@@ -56,16 +76,28 @@ export function Tasks() {
     return (
         <div>
             <h2>To-Do List</h2>
-            <ul>
+            <div className="list">
                 {tasks.map((task) => {
                     return (
-                        <li key={task.id}>{task.title}</li>
+                        <a key={task.id}>
+                            <input 
+                                type="checkbox" 
+                                checked={task.completed}
+                                onChange={() => editTask({ updatedTask: { ...task, completed: !task.completed } })}
+                            />
+                            <input 
+                                type="text" 
+                                value={task.title} 
+                                onChange={(e) => editTask({ updatedTask: { ...task, title: e.target.value } })}
+                            />
+                            <button onClick={() => deleteTask({ taskId: task.id })}>Del</button>
+                        </a>
                     )
 
                 })}
-            </ul>
+            </div>
 
-            <form action="" onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+            <form action="submit" onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
                 e.preventDefault();
                 const titleInput = e.currentTarget.elements.namedItem("title") as HTMLInputElement;
                 
@@ -74,7 +106,7 @@ export function Tasks() {
                 console.log("dodaję do listy: " + title)
                 
                 if (title) {
-                    addTask(title);
+                    addTask({ title });
                     titleInput.value = "";
                 }
 
